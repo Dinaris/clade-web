792502f4ac1f55ea7fc20b028df3a9e82ad8f361
david hockney <canozfuttu@hotmail.com>
2023-11-18 07:11:02 +0300

    improvementa

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,49 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    {
+      id: "worldcoin",
+      name: "Worldcoin",
+      type: "oauth",
+      wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid" } },
+      clientId: process.env.NEXT_PUBLIC_WORLDCOIN_CLIENT_ID,
+      clientSecret: process.env.WORLDCOIN_CLIENT_SECRET,
+      idToken: true,
+      profile(profile: any) {
+        return {
+          id: profile.sub,
+          name: profile.sub,
+        };
+      },
+    },
+    Auth0Provider({
+      clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
+      authorization: { params: { scope: "openid email name" } },
+      profile(profile: any, tokens: any) {
+        console.log("profile: ", profile);
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin";
+      return token;
+    },
+  },
+  debug: true,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/components/button/LoginButton.tsx
+++ src/components/button/LoginButton.tsx
@@ -1,15 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { Button } from "../ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useAccount } from "wagmi";
+import Image from "next/image";
+import Link from "next/link";
 
 const LoginButton = () => {
-  const session = useSession();
+  const dialogRef = useRef<HTMLDialogElement>(null)
+  const session = useSession()
+  const { open } = useWeb3Modal();
+  const { address } = useAccount();
+
+  console.log('session: ', session)
   return (
-    <Button onClick={!session.data?.user ? () => signIn("auth0") : () => signOut()} variant="outline">
+    <>
+    <dialog ref={dialogRef} className="relative p-8 rounded-2xl border-slate-300">
+      <div className="flex flex-col gap-6 mt-4">
+        <Button onClick={() => {open(); dialogRef.current?.close()}} className="p-6 flex justify-start gap-4" variant="outline">
+          <Image src="/images/walletconnect.png" alt="walletconnect" width={32} height={32} className="rounded-full" /> Connect with WalletConnect
+        </Button>
+        <Link href="/api/auth/signin" onClick={() => { dialogRef.current?.close(); }} className="p-6 flex justify-start gap-4">
+          <Image src="/images/worldcoin.png" alt="worldcoin" width={32} height={32} className="rounded-full" />Sign in with World ID
+        </Link>
+      </div>
+      <div onClick={() => dialogRef.current?.close()} className="absolute right-4 top-4 cursor-pointer">X</div>
+      <Button onClick={() => dialogRef.current?.close()} className="mt-4">Close</Button>
+    </dialog>
+    <Button onClick={!session .data?.user ? () => dialogRef.current?.showModal() : () => signOut()} variant="outline">
       {!session.data?.user ? "Login" : "Logout"}
     </Button>
+
+    </>
   );
 };
 
